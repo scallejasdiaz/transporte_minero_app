5959dfd16dc3decdd0170f5b342e0c979a41cb85
scallejasdiaz <sebastian.callejas@inacapmail.cl>
2025-06-27 21:26:10 -0400

    Actualización del proyecto - junio 2025

--- src/screens/PerfilScreen.tsx
+++ src/screens/PerfilScreen.tsx
@@ -12,8 +12,22 @@ export default function PerfilScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Perfil del Usuario</Text>
+      <View style={styles.profileContainer}>
+        <Text style={styles.label}>Nombre:</Text>
+        <Text style={styles.value}>Sebastián Callejas</Text>
+
+        <Text style={styles.label}>Rol:</Text>
+        <Text style={styles.value}>Trabajador de faena</Text>
+
+        <Text style={styles.label}>Correo electrónico:</Text>
+        <Text style={styles.value}>sebastian.callejas@example.com</Text>
+
+        <Text style={styles.label}>Empresa:</Text>
+        <Text style={styles.value}>Transporte Norte Minero S.A.</Text>
+      </View>
+
       <Button
-        title="Volver al Login"
+        title="Cerrar sesión"
         color="#f97315"
         onPress={() => navigation.navigate('Login')}
       />
@@ -33,5 +47,19 @@ const styles = StyleSheet.create({
     fontSize: 22,
     marginBottom: 20,
     fontWeight: 'bold',
+  },
+  profileContainer: {
+    marginBottom: 30,
+    width: '80%',
+  },
+  label: {
+    fontSize: 16,
+    color: '#666',
+    fontWeight: '600',
+  },
+  value: {
+    fontSize: 18,
+    color: '#242425',
+    marginBottom: 10,
   },
 });
